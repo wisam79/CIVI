1a384ed8a351df815619949a85620e29be17878b
wisam79 <73247302+wisam79@users.noreply.github.com>
2025-08-12 16:19:37 +0300

    Uploaded Successfully

---
+++ CIVI/.next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"components/toolbar.tsx -> html2canvas\":{\"id\":\"components/toolbar.tsx -> html2canvas\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_html2canvas_dist_html2canvas_js.js\"]}}"
