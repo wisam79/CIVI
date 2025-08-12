4d598945d38b4743738755df43b8e6af89125c0c
wisam79 <73247302+wisam79@users.noreply.github.com>
2025-08-12 16:17:57 +0300

    Uploaded Successfully

---
+++ CIVI/next.config.ts
@@ -0,0 +1,30 @@
+import type {NextConfig} from 'next';
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
